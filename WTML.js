ee0e5273e0313e12161bba65462ccf22c6163653
Zak Henry <zak.henry@gmail.com>
2013-09-19 19:01:38 +1000

    Created a barebones functional framework to begin development on

---
+++ WTML.js
@@ -0,0 +1,26 @@
+/**
+ * WTML.js
+ * A WTML language parser in javascript. Converts wtml files to html files
+ *
+ * @author Zak Henry - 2013
+ */
+
+
+
+;
+(function(){
+
+    /**
+     *
+     * @constructor
+     */
+    this.WTMLTranslator = function(){
+
+        this.translate = function(inputWTML){
+            return inputWTML;
+        };
+
+        return this;
+    };
+
+})();
