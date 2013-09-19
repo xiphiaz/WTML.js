5e16859c872288ddbd478c635170560973d10daf
Zak Henry <zak.henry@gmail.com>
2013-09-19 19:21:57 +1000

    Broke down the interpreter into lexer/parser/compiler pattern

--- WTML.js
+++ WTML.js
@@ -16,8 +16,57 @@
      */
     this.WTMLTranslator = function(){
 
+        var Lexer = (function(){ //break down the string into elements
+            return function(){
+                this.process = function(wtmlRaw){
+                    var elements = [];
+
+                    elements.push(wtmlRaw);
+
+                    return elements;
+                }
+            };
+        })();
+
+        var Parser = (function(){ //build up the syntax tree with the elements
+            return function(){
+                this.process = function(elements){
+                    var domTree = {};
+
+                    domTree.test = elements[0];
+
+                    return domTree;
+                }
+            };
+        })();
+
+        var Compiler = (function(){ //build up the syntax tree with the elements
+            return function(){
+                this.process = function(domTree){
+                    var html = '';
+
+                    html = domTree.test;
+
+                    return html;
+                }
+            };
+        })();
+
         this.translate = function(inputWTML){
-            return inputWTML;
+
+            var lexer = new Lexer(),
+                parser = new Parser(),
+                compiler = new Compiler()
+            ;
+
+            var elements = lexer.process(inputWTML),
+                domTree = parser.process(elements)
+            ;
+
+            console.log('elements', elements);
+            console.log('domTree', domTree);
+
+            return compiler.process(domTree);
         };
 
         return this;
