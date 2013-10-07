b550fbe71e7b2c6eee0d26f4e0babb5fcb390f77
Zak Henry <zak.henry@gmail.com>
2013-10-07 14:45:20 +1100

    Completed html compilation, with tab delimiting and correctly placed newlines

--- WTML.js
+++ WTML.js
@@ -44,10 +44,10 @@
                             if (typeof matches[4] !== 'undefined'){ //id
                                 element.selector.id = matches[4].substr(1);
                             }
-                            if (typeof matches[5] !== 'undefined'){ //class
+                            if (typeof matches[5] !== 'undefined' && matches[5] !== ""){ //class
                                 element.selector.class = matches[5].substr(1).split('.');
                             }
-                            if (typeof matches[6] !== 'undefined'){ //attr
+                            if (typeof matches[6] !== 'undefined' && matches[6] !== ""){ //attr
                                 element.selector.attr = matches[6].slice(1, -1).split(/\].*\[/);
                             }
                             if (typeof matches[7] !== 'undefined'){ //content
@@ -103,69 +103,6 @@
         })();
 
 
-        /*
-        Example DOM tree:
-         */
-
-        var domTreeExample = {
-            children:[
-                {
-                    element: {},
-                    children: [
-                        {
-                            element: {
-                                tag: 'div',
-                                id: 'example'
-                                /* ... etc */
-                            },
-                            children: [
-
-                            ]
-                        },
-                        {
-                            element: {
-                                tag: 'span',
-                                id: 'example2'
-                            },
-                            children: [
-
-                            ]
-                        },
-                        {
-                            element: {
-                                tag: 'code',
-                                id: 'example3'
-                            },
-                            children: [
-
-                            ]
-                        }
-                        /* ... etc */
-                    ]
-                }
-            ],
-            locate: function(locateKey){
-                var retrieveObject = this;
-
-                while(locateKey.length > 0){
-                    var key = locateKey.shift();
-                    console.log('key', key);
-                    console.log('retrieveObject', retrieveObject)
-                    retrieveObject = retrieveObject.children[key]; //drill down into object
-                }
-
-                return retrieveObject;
-            }
-        };
-
-        var domAccessExample = domTreeExample.children[0].children[2].element.tag; //div
-        var accessKey = [0, 2];
-
-        domTreeExample.locate([0, 2]).element.class = "foobar";
-
-        console.log('example access: ', domAccessExample);
-        console.log('locate example:', domTreeExample.locate([0, 2]));
-
         var Parser = (function(){ //build up the syntax tree with the elements
             return function(){
                 this.process = function(elements){
@@ -217,7 +154,7 @@
 
                     }
 
-                    domTree.test = elements[0];
+                    domTree.test = JSON.stringify(domTree.children, null, "\t");
 
                     return domTree;
                 }
@@ -226,10 +163,162 @@
 
         var Compiler = (function(){ //build up the html from the syntax tree
             return function(){
+
+                var htmlElements = [];
+
+                var compileSelector = function(selector, hasChildNodes){
+
+                    var startString = '<';
+                    var htmlNode = {};
+
+
+                    startString += selector.tag;
+
+                    if (typeof selector.id !== 'undefined'){
+                        startString += ' id="'+selector.id+'"';
+                    }
+
+                    if (typeof selector.class !== 'undefined'){
+                        startString += ' class="'+selector.class.join(' ')+'"';
+                    }
+
+                    if (typeof selector.attr !== 'undefined'){
+                        startString += ' '+selector.attr.join(' ');
+                    }
+
+                    if (!hasChildNodes && typeof selector.content =='undefined'){ //no children node or content, self close
+                        startString += '/>';
+                    }else{
+                        startString += '>';
+                        htmlNode.closeTag = '</'+selector.tag+'>';
+                    }
+
+                    if (typeof selector.content !=='undefined'){
+                        startString += selector.content;
+                    }
+
+                    htmlNode.openTag = startString;
+
+                    return htmlNode;
+                };
+
+                var previousNodeDepth = 0;
+                var closingTagBuffer = [];
+                var appendClosingTags = function(referenceDepth){
+                    var depthDiff =  previousNodeDepth - referenceDepth;
+
+                    if (depthDiff == 0
+                        && typeof htmlElements[htmlElements.length-1] !== 'undefined'
+                        && htmlElements[htmlElements.length-1].hasClosingTag === false
+                        ){
+                        return; //the prior sibling has no closing tag, don't try to close it
+                    }
+
+
+                    if (closingTagBuffer.length > 0 && depthDiff >= 0){
+                        var retrievedElements = closingTagBuffer.splice(0, depthDiff + 1);
+                        console.log('retrievedElements', retrievedElements, 'depth diff was ', depthDiff);
+                        htmlElements = htmlElements.concat(retrievedElements);
+                    }
+                };
+                var nodeReferenceId = 0;
+                var processNode = function(node, hasChildNodes, currentDepth){
+                    console.log(node);
+
+
+                    if(typeof node.element == 'undefined'){
+                        return;
+                    }
+                    if (node.element.type == 'selector'){
+                        var selector = compileSelector(node.element.selector, hasChildNodes);
+
+                        htmlElements.push({
+                            refId: nodeReferenceId,
+                            content: selector.openTag,
+                            depth: currentDepth,
+                            hasClosingTag: (typeof selector.closeTag !== 'undefined')
+                        });
+
+                        if (typeof selector.closeTag !== 'undefined'){
+
+                            closingTagBuffer.unshift({
+                                refId: nodeReferenceId,
+                                content: selector.closeTag,
+                                depth: currentDepth,
+                                hasClosingTag: null //it IS the closing tag!
+                            });
+                        }
+                    }
+
+                    nodeReferenceId ++;
+
+                    console.log('this node value', node.element.value, 'at depth', currentDepth);
+                    previousNodeDepth = currentDepth;
+                };
+
+                var walkTree = function(node, currentDepth){
+
+
+                    appendClosingTags(currentDepth);
+                    if (typeof node.children == 'undefined'){
+                        processNode(node, false, currentDepth);
+                        return; //end of tree reached
+                    }else{
+                        processNode(node, true, currentDepth);
+                    }
+                    for (var i = 0; i < node.children.length; i++){
+                        if (typeof node.children[i] == 'undefined'){
+                            continue;
+                        }
+
+                        walkTree(node.children[i], currentDepth + 1);
+                    }
+                };
+
+
+                var repeatString = function(pattern, count) {
+                    if (count < 1) return '';
+                    var result = '';
+                    while (count > 0) {
+                        if (count & 1) result += pattern;
+                        count >>= 1;
+                        pattern += pattern;
+                    }
+                    return result;
+                };
+
+                var formatHtml = function(htmlElements){
+                    var prevReferenceId = -1; //init
+                    var htmlFormatted = [];
+                    for (var i=0; i<htmlElements.length; i++){
+                        var thisElement = htmlElements[i];
+                        var referenceId = thisElement.refId;
+
+                        if (referenceId !== prevReferenceId){
+                            htmlFormatted.push(repeatString('\u00A0', thisElement.depth*4) + thisElement.content);
+                            htmlFormatted.push('\n');
+                        }else{
+                            htmlFormatted.pop(); //remove the last \n
+                            htmlFormatted.push(thisElement.content + '\n');
+                        }
+
+                        prevReferenceId = referenceId; //for the next loop
+                    }
+                    return htmlFormatted.join('');
+                };
+
                 this.process = function(domTree){
                     var html = '';
 
-                    html = domTree.test;
+                    walkTree(domTree, 0);
+                    appendClosingTags(0); //final tag pop
+
+                    console.log(htmlElements);
+                    html = formatHtml(htmlElements); //this needs to be ooped better
+
+                    console.log(htmlElements);
+                    console.log(closingTagBuffer);
+                    console.log(html);
 
                     return html;
                 }
@@ -247,8 +336,8 @@
                 domTree = parser.process(elements)
             ;
 
-            console.log('elements', elements);
-            console.log('domTree', domTree);
+//            console.log('elements', elements);
+//            console.log('domTree', domTree);
 
             return compiler.process(domTree);
         };
